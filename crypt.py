9b2cd460d90ed6adb56179d40f62229f52a4198b
moskwam <marcin.moskwa96@gmail.com>
2018-05-23 17:48:46 +0200

    Modified both encryption and decryption.

--- crypt.py
+++ crypt.py
@@ -1,61 +1,70 @@
 from PIL import Image
 
-def load_secret_image(source="politechnika.bmp"):
-    image = Image.open(source)
-    x_len, y_len = image.size
+def load_source_image(path):
+    image = Image.open(path)
+    size = image.size
+    pixel_list = list(image.getdata())
+    return size, pixel_list
 
-    pixel_list = []
+def load_secret_image(path):
+    image = Image.open(path)
+    size = image.size
+    pixel_list = list(image.getdata())
+    palette = image.getpalette()
+    return size, pixel_list, palette
 
-    for i in range(0, y_len):
-        for j in range(0, x_len):
-            pixel = image.getpixel((j, i))
-            pixel_list.append('{0:08b}'.format(pixel))
-
-    return pixel_list
-
-def calculate_new_indexes(x_len, y_len, old_x_indx, old_y_indx):
-    if old_x_indx == x_len-1:
-        x_indx = 0
-        y_indx = old_y_indx + 1
-    else:
-        x_indx = old_x_indx + 1
-        y_indx = old_y_indx
-    return x_indx, y_indx
-
-if __name__ == "__main__":
-    pixel_list = load_secret_image()
-
-    image = Image.open("duck.bmp")
-    x_len, y_len = image.size
-
-    x_indx = 0
-    y_indx = 0
+def encrypt_part(source_pixel_list, secret_part, source_index=0):
+    #secret_part is either palette or secret image pixel list
 
     color_list = []
     index_list = []
 
-    for pixel in pixel_list:
-        for i in range(0, 3):
-            index_list.append((x_indx, y_indx))
-            r, g, b = image.getpixel((x_indx, y_indx))
+    for value in secret_part:
+        binary_value = '{0:08b}'.format(value)
+
+        #fetching 3 next pixels of source image
+        for i in range(0,3):
+            index_list.append(source_index)
+            r, g, b = source_pixel_list[source_index]
             color_list.append('{0:08b}'.format(r))
             color_list.append('{0:08b}'.format(g))
             color_list.append('{0:08b}'.format(b))
-            x_indx, y_indx = calculate_new_indexes(x_len, y_len, x_indx, y_indx)
+            source_index += 1
 
-        #faza podmiany
+        #changing the last bit of source pixels
         for i in range(0, 9):
             if i < 8:
-                #ostatni element listy color_list jest pomijany
+                #the last element of the color_list is omitted
                 tmp_color_value = list(color_list[i])
-                tmp_color_value[7] = pixel[i]
+                tmp_color_value[7] = binary_value[i]
                 color_list[i] = "".join(tmp_color_value)
             color_list[i] = int(color_list[i], 2)
 
-        #faza kodowania
+        #save results in list of source image pixels
         for i in range (0, 3):
             rgb = tuple(color_list[0:3])
             del color_list[0:3]
-            image.putpixel(index_list.pop(0), rgb)
+            source_pixel_list[index_list.pop(0)] = rgb
 
-    image.save("duck-crypt.bmp")
+    return source_pixel_list, source_index
+
+def encrypt(source_image_path, secret_image_path):
+    source_image_size, source_pixel_list = load_source_image(source_image_path)
+    secret_image_size, secret_pixel_list, secret_palette = load_secret_image(secret_image_path)
+
+    #encrypting the palette
+    source_pixel_list, source_index = encrypt_part(source_pixel_list, secret_palette)
+
+    #encrypting the pixels
+    source_pixel_list, source_index = encrypt_part(source_pixel_list, secret_pixel_list, source_index)
+
+    encrypted_image = Image.new("RGB", source_image_size)
+    encrypted_image.putdata(source_pixel_list)
+    encrypted_image.save("duck-crypt.bmp")
+
+    return (secret_image_size[0], secret_image_size[1], len(secret_palette))
+
+if __name__ == "__main__":
+    # Key can be used to reveal the secret image. It can be also sent to a receiver
+    # using the RSA cryptography.
+    key = encrypt("duck.bmp", "politechnika.bmp")
--- decrypt.py
+++ decrypt.py
@@ -1,32 +1,26 @@
 from PIL import Image
 
-if __name__ == "__main__":
-    #kluczem do odszyfrowania jest para liczb x_indx, y_indx
-    image = Image.open("duck-crypt.bmp")
-    test_image = Image.open("politechnika.bmp")
-    img_palette = test_image.getpalette()
-    # print(len(img_palette))
-    # print(img_palette)
-    x_len, y_len = image.size
-    x_indx, y_indx = 512, 500
+def load_stego_image(path):
+    image = Image.open(path)
+    pixel_list = list(image.getdata())
+    return pixel_list
 
+def decrypt_part(stego_pixel_list, stego_image_start_index, stego_image_end_index):
+    #used to decrypt both palette and pixel list
     single_color = []
-    secret_image_pixel_list = []
+    secret_part = []
 
-    for i in range(0, y_indx+1):
-        for j in range(0, x_len):
-            if i == y_indx and j == x_indx:
-                break
-            r, g, b = image.getpixel((j, i))
+    for i in range(stego_image_start_index, stego_image_end_index):
+            r, g, b = stego_pixel_list[i]
             r_byte = ('{0:08b}'.format(r))
             g_byte = ('{0:08b}'.format(g))
             b_byte = ('{0:08b}'.format(b))
             if len(single_color) == 6:
-                #z poprzednich dwóch iteracji
+                #from two previous iterations
                 single_color.append(r_byte[-1])
                 single_color.append(g_byte[-1])
                 pixel_string = ''.join(single_color)
-                secret_image_pixel_list.append(int(pixel_string, 2))
+                secret_part.append(int(pixel_string, 2))
                 single_color = []
                 pixel_string = ''
             else:
@@ -34,6 +28,34 @@ if __name__ == "__main__":
                 single_color.append(g_byte[-1])
                 single_color.append(b_byte[-1])
 
-    new_image = Image.new("P", (448, 298))
-    new_image.putdata(secret_image_pixel_list)
-    new_image.show()
+    return secret_part
+
+def decrypt(key, stego_image_path="duck-crypt.bmp"):
+    # In order to reveal the secret one has to pass size of the secret and the palette.
+    # Those can be sent safely, for example, using RSA cryptography.
+    secret_image_height = key[0]
+    secret_image_width =  key[1]
+    secret_image_palette_size = key[2]
+
+    secret_image_size = secret_image_height * secret_image_width
+    stego_pixel_list = load_stego_image(stego_image_path)
+
+    stego_image_end_index = secret_image_palette_size*3
+    secret_image_palette = decrypt_part(stego_pixel_list, 0, stego_image_end_index)
+
+    stego_image_start_index = stego_image_end_index
+    stego_image_end_index = stego_image_start_index + 3*secret_image_size
+    secret_image_pixel_list = decrypt_part(stego_pixel_list, stego_image_start_index, stego_image_end_index)
+
+    secret_image = Image.new("P", (secret_image_height, secret_image_width))
+    secret_image.putdata(secret_image_pixel_list)
+    secret_image.putpalette(secret_image_palette)
+    secret_image.show()
+
+if __name__ == "__main__":
+    key = (448, 298, 768)
+    # first - height of the secret image
+    # second - width of the secret image
+    # third - size of the palette
+
+    decrypt(key)
